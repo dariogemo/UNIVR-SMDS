bfbb0365d1eadff8c51e4d4a12793e28b39c9a6c
Dario Gemo <dario.gemo@studenti.univr.it>
2025-01-13 17:43:15 +0100

    lag plots. autocorrelation plots of GDPs. Data split in train and test. FFT and PSD. Visual stationarity check and statistics stationarity check. log transform, seasonal (13) and first order differencing to achieve stationarity. added needed functions to functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from statsmodels.tsa.stattools import adfuller, kpss
 
 def create_df(df):
     df = df.reset_index().drop(['Country', 'CountryID'], axis = 1)
@@ -20,7 +21,46 @@ def highest_corr_variable(corr_list, max_list):
     df = pd.DataFrame(max_list, index = ['Italy', 'Japan', 'UAE', 'Canada', 'South Africa'], columns = ['Highest correlation variable'])
     display(df)
 
-def prova(corr_df):
+def lowest_corr_variable(corr_df):
     cgdpd = corr_df.drop('GDP', axis = 1).replace({1 : np.nan})
     idx = cgdpd.stack().idxmin()
     return idx
+
+def train_test_split(df):
+    train = df.loc[:2009]
+    train = train.astype('float')
+    test = df.loc[2010:]
+    test = test.astype('float')
+    lista = [train, test]
+    return lista
+
+def adfuller_test(data):
+  adf_test = adfuller(data, autolag='AIC') # AIC is the default option
+  print('ADF Statistic:', adf_test[0])
+  print('p-value: ', adf_test[1])
+  print('Critical Values:')
+  for key, value in adf_test[4].items():
+      print('\t%s: %.3f' % (key, value))
+  if adf_test[1] <= 0.05:
+    print('We can reject the null hypothesis (H0) --> data is stationary')
+  else:
+    print('We cannot reject the null hypothesis (H0) --> data is non-stationary')
+
+def kpss_test(data):
+  kpss_out = kpss(data,regression='c', nlags='auto', store=True)
+  print('KPSS Statistic:', kpss_out[0])
+  print('p-value: ', kpss_out[1])
+  if kpss_out[1] <= 0.05:
+    print('We can reject the null hypothesis (H0) --> unit root, data is not stationary')
+  else:
+    print('We cannot reject the null hypothesis (H0) --> data is trend stationary')
+
+def log_transform(df_train_test):
+    for nation in df_train_test:
+        df_train_test[nation] = (np.log(df_train_test[nation][0]), np.log(df_train_test[nation][1]))
+    return df_train_test
+
+def difference(df_train_test, difference):
+    for nation in df_train_test:
+        df_train_test[nation] = (df_train_test[nation][0].diff(difference).dropna(), df_train_test[nation][1].diff().dropna())
+    return df_train_test
